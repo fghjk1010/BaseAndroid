a3ebc7008de5a6ff1af2debb14d9b85fedd4c3a1
fghjk1010 <scyshya@126.com>
2015-03-10 22:15:55 +0800

    test12

--- BaseAndroid/src/com/yulian/framework/view/swipe/xswipelistview/Test1.java
+++ BaseAndroid/src/com/yulian/framework/view/swipe/xswipelistview/Test1.java
@@ -3,5 +3,6 @@ package com.yulian.framework.view.swipe.xswipelistview;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("test11");
+		System.out.println("test12");
 	}
 }
